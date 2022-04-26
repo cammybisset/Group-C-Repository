76eab4aca40d76ed3f7261294ebb1f77c7540342
cbisset <40525505@live.napier.ac.uk>
2022-04-26 17:49:06 +0100

    Initial Java Uploads

--- src/main/java/com/napier/sem/data/Country.java
+++ src/main/java/com/napier/sem/data/Country.java
@@ -1,4 +1,26 @@
 package com.napier.sem.data;
 
+/**
+ * Class containing Country fields and variables
+ */
 public class Country {
+    public String Code;
+    public String Name;
+    public String Continent;
+    public String Region;
+    public double SurfaceArea;
+    public int IndepYear;
+    public long Population;
+    public long InCityPop;
+    public long OutCityPop;
+    public long InCityPerc;
+    public long OutCityPerc;
+    public double LifeExpectancy;
+    public double GNP;
+    public double GNPOld;
+    public String LocalName;
+    public String GovernmentForm;
+    public String HeadOfState;
+    public int Capital;
+    public String Code2;
 }
--- src/main/java/com/napier/sem/data/Language.java
+++ src/main/java/com/napier/sem/data/Language.java
@@ -1,4 +1,10 @@
 package com.napier.sem.data;
 
+/**
+ * Class containing Language and variables
+ */
 public class Language {
+    public String Language;
+    public double TotalSpeakers;
+    public double PercentOfWorldPop;
 }
