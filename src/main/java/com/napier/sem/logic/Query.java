f3d64174a5d1c7ba17eb6bae1f844a66d3a4b23f
cbisset <40525505@live.napier.ac.uk>
2022-04-27 21:20:57 +0100

    DB Query 1

--- src/main/java/com/napier/sem/logic/Query.java
+++ src/main/java/com/napier/sem/logic/Query.java
@@ -1,4 +1,30 @@
 package com.napier.sem.logic;
 
-public class Query {
-}
+import com.napier.sem.data.City;
+import com.napier.sem.data.Country;
+import com.napier.sem.data.Language;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ * Queries Produced from the database
+ */
+public class Query
+    {
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT NAME, population "
+                            + "FROM country c "
+                            + "ORDER BY 2 DESC ";
+    // Execute SQL statement
+    ResultSet rset;
+            {
+        Country cou = new Country();
+        cou.Name = rset.getString("NAME");
+        cou.Population = rset.getInt("Population");
+            }
+    }
